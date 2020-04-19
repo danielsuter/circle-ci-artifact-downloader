6d63784b203cb7f764d627608a470c873ae70f7d
danielsuter <suter_daniel@hotmail.com>
2020-04-19 14:22:52 +0200

    Extract configuration.

--- src/main/java/ch/danielsuter/circleci/App.java
+++ src/main/java/ch/danielsuter/circleci/App.java
@@ -1,42 +1,55 @@
 package ch.danielsuter.circleci;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class App {
-    private final static String targetPath = "C:\\Users\\suter\\Google Drive\\Shinsei Kan Lenzburg\\Website\\CRM";
 
     public static void main(String[] args) {
-        String token = "TODO";
 
-        CircleCiApi api = new CircleCiApi(token, "bitbucket", "Sanix", "shinseikan-crm", "develop");
+        try (FileInputStream configStream = new FileInputStream("configuration.properties")) {
+            Properties configuration = new Properties();
+            configuration.load(configStream);
 
-        List<Build> builds = api.getRecentBuilds(0, 100, "succesful");
-        List<Build> backups = builds.stream()
-                .filter(build -> build.workflows.workflowName.equals("database_backup"))
-                .filter(build -> build.startTime.isAfter(LocalDate.of(2019, 8, 9)))
-                .collect(Collectors.toList());
-        System.out.println("Number of backups: " + backups.size());
+            String targetPath = configuration.getProperty("target_path");
+            String token = configuration.getProperty("token");
+            String vcs = configuration.getProperty("vcs");
+            String user = configuration.getProperty("user");
+            String project = configuration.getProperty("project");
+            String branch = configuration.getProperty("branch");
+            CircleCiApi api = new CircleCiApi(token, vcs, user, project, branch);
 
-        List<Artifact> artifacts = backups.parallelStream()
-                .map(backup -> backup.buildNumber)
-                .map(buildNumber -> api.getArtifacts(buildNumber).get(0))
-                .collect(Collectors.toList());
-        System.out.println("Number of urls: " + artifacts.size());
+            List<Build> builds = api.getRecentBuilds(0, 100, "successful");
+            List<Build> backups = builds.stream()
+                    .filter(build -> build.workflows.workflowName.equals("database_backup"))
+                    .filter(build -> build.startTime.isAfter(LocalDate.of(2020, 4, 12)))
+                    .collect(Collectors.toList());
+            System.out.println("Number of backups: " + backups.size());
 
-        artifacts.stream()
-                .filter(App::notExists)
-                .forEach(artifact -> download(api, artifact));
+            List<Artifact> artifacts = backups.parallelStream()
+                    .map(backup -> backup.buildNumber)
+                    .map(buildNumber -> api.getArtifacts(buildNumber).get(0))
+                    .collect(Collectors.toList());
+            System.out.println("Number of urls: " + artifacts.size());
 
+            artifacts.stream()
+                    .filter(artifact -> notExists(artifact, targetPath))
+                    .forEach(artifact -> download(api, artifact, targetPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void download(CircleCiApi api, Artifact artifact) {
+    private static void download(CircleCiApi api, Artifact artifact, String targetPath) {
         api.downloadArtifact(artifact, targetPath);
     }
 
-    private static boolean notExists(Artifact artifact) {
+    private static boolean notExists(Artifact artifact, String targetPath) {
         String cleanFileName = cleanFileName(artifact.path);
 
         return !new File(targetPath, cleanFileName).exists();
